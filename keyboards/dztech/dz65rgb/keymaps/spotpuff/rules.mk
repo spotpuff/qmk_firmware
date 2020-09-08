e8d534afb36900a01c65e76a7f4a40fb273a9ddd
Philip Chan <Philip_Chan@wsib.on.ca>
2020-09-08 16:58:43 -0400

    Initial keymap for dz65rgb

---
+++ keyboards/dztech/dz65rgb/keymaps/spotpuff/rules.mk
@@ -0,0 +1,5 @@
+BOOTMAGIC_ENABLE = no	# disable bootmagic; causes confusion when people boot up with a key held down accidentally
+#VELOCIKEY_ENABLE = yes #don't know
+# AUTO_SHIFT_ENABLE = yes      # Auto Shift
+
+#LTO_ENABLE = yes #don't know what this is
