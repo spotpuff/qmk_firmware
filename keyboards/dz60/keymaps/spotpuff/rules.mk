22caf9f64ec5aca942c50c7338f309cec50f3379
Philip Chan <spotpuff@gmail.com>
2020-09-23 02:48:51 -0400

    fixed rules.mk rebase problems

--- keyboards/dz60/keymaps/spotpuff/rules.mk
+++ keyboards/dz60/keymaps/spotpuff/rules.mk
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
-=======
 BOOTMAGIC_ENABLE = no
->>>>>>> f079dd90ed88fd29b7e5bbdd3496742f3afeff4e
-=======
-BOOTMAGIC_ENABLE = no
->>>>>>> f079dd90ed88fd29b7e5bbdd3496742f3afeff4e
