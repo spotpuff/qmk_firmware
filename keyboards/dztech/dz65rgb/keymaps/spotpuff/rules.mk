fac34b2fd8211b7a046b6090969d72186b9a955e
Philip Chan <spotpuff@gmail.com>
2020-09-23 03:06:03 -0400

    Updated dz65RGB rules.mk

--- keyboards/dztech/dz65rgb/keymaps/spotpuff/rules.mk
+++ keyboards/dztech/dz65rgb/keymaps/spotpuff/rules.mk
@@ -1,4 +1,10 @@
-BOOTMAGIC_ENABLE = no	# disable bootmagic; causes confusion when people boot up with a key held down accidentally
+# Build Options
+# override the options specified in dz60/rules.mk
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
+
 #VELOCIKEY_ENABLE = yes #don't know
 # AUTO_SHIFT_ENABLE = yes      # Auto Shift
 
