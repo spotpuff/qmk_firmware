f5cb3ff794908b771cd680af1a94219787645e95
Philip Chan <Philip_Chan@wsib.on.ca>
2020-09-23 03:34:52 -0400

    Added F keys to media layer to make it easier to hit F keys.

--- keyboards/dztech/dz65rgb/keymaps/spotpuff/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/spotpuff/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* SFX/Multimedia/Numpad layer
  * ,-----------------------------------------------------------------------------------------------.
- * |     |     |     |     |     |     |NumLk|     |  /  |  *  |  -  |  =  |     |    Trns   |Trns |
+ * |     | F1  | F2  | F3  | F4  |     |NumLk|     |  /  |  *  |  -  |  =  |     |    Trns   |Trns |
  * |-----------------------------------------------------------------------------------------------+
  * |        |     | Prev | Play| Next|     |     |  7  |  8  |  9  |  +  |     |     | RESET |     |
  * |-----------------------------------------------------------------------------------------------+
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 	[_MEDIA] = LAYOUT_65_ansi(
-		XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK,   KC_NO, KC_PSLS, KC_PAST, KC_PMNS,  KC_PEQL,  XXXXXXX, _______, _______, \
+		XXXXXXX, KC_F1,    KC_F2,   KC_F3,   KC_F4,   XXXXXXX, KC_NLCK,   KC_NO, KC_PSLS, KC_PAST, KC_PMNS,  KC_PEQL,  XXXXXXX, _______, _______, \
 		XXXXXXX, XXXXXXX,  KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,   KC_P7,   KC_P8,   KC_P9, KC_PPLS,  XXXXXXX,  XXXXXXX,   RESET, _______, \
 		_______, XXXXXXX,  KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PPLS,  XXXXXXX,           _______, _______, \
 		_______, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, KC_PENT,  XXXXXXX,           XXXXXXX, XXXXXXX, \
