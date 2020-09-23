0c6f404b990f45c0c19571918d8a2db2c96074cc
Philip Chan <spotpuff@gmail.com>
2020-09-23 03:04:48 -0400

    updated coppertop rules.mk

--- keyboards/dz60/keymaps/coppertop/rules.mk
+++ keyboards/dz60/keymaps/coppertop/rules.mk
@@ -1,1 +1,6 @@
-BOOTMAGIC_ENABLE = no
+# Build Options
+# override the options specified in dz60/rules.mk
+BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
+
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover
