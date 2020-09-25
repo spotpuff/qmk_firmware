b3cdf42752e79444951166ef5c39bd3e5214010d
Spotpuff <spotpuff@gmail.com>
2020-09-25 04:17:38 -0400

    Keymap changes and graphic update

--- keyboards/dztech/dz65rgb/keymaps/spotpuff/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/spotpuff/keymap.c
@@ -62,41 +62,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------------+
  * |  Caps  |     |R_HUD|R_SAD|     |      |     |     |     |     |SLCK |PAUSE|  App/Menu   |     |
  * |-----------------------------------------------------------------------------------------------+
- * |  Trns    |R_M_P|R_M_B|R_M_R|R_M_SW|R_M_S|R_M_K|R_M_X|R_M_G|     |     |     |     |BLUP |     |
+ * |  Trns    |R_M_P|R_M_B|R_M_R|R_M_SW|R_M_S|R_M_K|R_M_X|R_M_G|     |     |           |     |     |
  * |-----------------------------------------------------------------------------------------------+
- * |  Trns | Trns  | Trns  |                                   |     |     |     |BLTog|BLDN |BLInc|
+ * |  Trns | Trns  | Trns  |                                   |     |     |     |     |     |     |
  *  `----------------------------------------------------------------------------------------------'
  */
 
 	[_FN] = LAYOUT_65_ansi(
-		XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  XXXXXXX,  KC_INS, \
-		XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  KC_CAD, _______, \
-		KC_CAPS, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,            KC_APP, _______, \
-		_______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, XXXXXXX, XXXXXXX, XXXXXXX,           _______, _______, \
-		_______, _______, _______,                     _______,                            _______, _______, _______,  _______, _______, _______  \
+		XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,  XXXXXXX, KC_INS,  \
+		XXXXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_CAD,  XXXXXXX, \
+		KC_CAPS, XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SLCK, KC_PAUS,           KC_APP,  XXXXXXX, \
+		_______, RGB_M_P, RGB_M_B, RGB_M_R, RGB_M_SW, RGB_M_SN, RGB_M_K, RGB_M_X, RGB_M_G, XXXXXXX, XXXXXXX, XXXXXXX,           XXXXXXX, XXXXXXX, \
+		_______, _______, _______,                    _______,                             _______, _______, _______,  XXXXXXX, XXXXXXX, XXXXXXX  \
 	),
 
 	/* SFX/Multimedia/Numpad layer
  * ,-----------------------------------------------------------------------------------------------.
- * |     |     |     |     |     |     |NumLk|     |  /  |  *  |  -  |  =  |     |    Trns   |Trns |
+ * |     | F1  | F2  | F3  | F4  |     |NumLk|     |  /  |  *  |  -  |  =  |     |    Trns   |Trns |
  * |-----------------------------------------------------------------------------------------------+
  * |        |     | Prev | Play| Next|     |     |  7  |  8  |  9  |  +  |     |     | RESET |     |
  * |-----------------------------------------------------------------------------------------------+
  * |  Trns   |     |Mute | VUp | VDn |     |     |  4  |  5  |  6  |  +  |     |    Trns     |     |
  * |-----------------------------------------------------------------------------------------------+
- * |  Trns     |     |     |     |     |     |     |  1  |  2  |  3  |NumEN|     |     |     |     |
+ * |  Trns     |     |     |     |     |     |     |  1  |  2  |  3  |NumEN|           |     |     |
  * |-----------------------------------------------------------------------------------------------+
- * | Trns | Trns  | Trns  |                 0                  |  .  |     |     |     |     |     |
+ * | Trns | Trns  | Trns  |                 0                 |  .  |     |      |     |     |     |
  *  `----------------------------------------------------------------------------------------------'
  */
 
 
 	[_MEDIA] = LAYOUT_65_ansi(
-		XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_NLCK,   KC_NO, KC_PSLS, KC_PAST, KC_PMNS,  KC_PEQL,  XXXXXXX, XXXXXXX, XXXXXXX, \
-		XXXXXXX, XXXXXXX,  KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,   KC_P7,   KC_P8,   KC_P9, KC_PPLS,  XXXXXXX,  XXXXXXX,   RESET, XXXXXXX, \
-		_______, XXXXXXX,  KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PPLS,  XXXXXXX,           XXXXXXX, XXXXXXX, \
-		_______, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, KC_PENT,  XXXXXXX,           XXXXXXX, XXXXXXX, \
-		_______, _______,  _______,                     KC_P0,                            KC_PDOT, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX  \
+		XXXXXXX, KC_F1,    KC_F2,   KC_F3,   KC_F4,   XXXXXXX, KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS, KC_PEQL,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, \
+		XXXXXXX, XXXXXXX,  KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, KC_P7,   KC_P8,   KC_P9,   KC_PPLS,  XXXXXXX,  XXXXXXX, RESET,   XXXXXXX, \
+		_______, XXXXXXX,  KC_MUTE, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,  XXXXXXX,           XXXXXXX, XXXXXXX, \
+		_______, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_PENT,  XXXXXXX,           XXXXXXX, XXXXXXX, \
+		_______, _______,  _______,                   KC_P0,                              KC_PDOT, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX  \
 	),
 
 };
